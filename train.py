461d389119c6ee708634a5d320f15518cc1b76ab
tak <tak.tak@kyp.biglobe.ne.jp>
2021-01-23 18:54:28 +0900

    Update train.py

--- train.py
+++ train.py
@@ -174,8 +174,8 @@ def setup_training_options(
     if cfg == 'auto':
         desc += f'{gpus:d}'
         spec.ref_gpus = gpus
-        spec.mb = max(min(gpus * min(4096 // res, 32), 64), gpus) # keep gpu memory consumption at bay
-#        spec.mb = max(min(gpus * min(4096 // res, 1), 1), gpus) # keep gpu memory consumption at bay
+#        spec.mb = max(min(gpus * min(4096 // res, 32), 64), gpus) # keep gpu memory consumption at bay
+        spec.mb = max(min(gpus * min(4096 // res, 1), 1), gpus) # keep gpu memory consumption at bay
         spec.mbstd = min(spec.mb // gpus, 4) # other hyperparams behave more predictably if mbstd group size remains fixed
         spec.fmaps = 1 if res >= 512 else 0.5
         spec.lrate = 0.002 if res >= 1024 else 0.0025
