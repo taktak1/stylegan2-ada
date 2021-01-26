636229a5cd2a8d7e007929e350d9d1e11101a32b
tak <tak.tak@kyp.biglobe.ne.jp>
2021-01-26 10:08:45 +0900

    Update train.py

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+
 # Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
 #
 # NVIDIA CORPORATION and its licensors retain all intellectual property
@@ -28,39 +29,40 @@ class UserError(Exception):
 
 #----------------------------------------------------------------------------
 
-def setup_training_options(
-    # General options (not included in desc).
-    gpus       = None, # Number of GPUs: <int>, default = 1 gpu
-    snap       = None, # Snapshot interval: <int>, default = 50 ticks
+def setup_training_options():
 
-    # Training dataset.
-    data       = None, # Training dataset (required): <path>
-    res        = None, # Override dataset resolution: <int>, default = highest available
-    mirror     = True, # Augment dataset with x-flips: <bool>, default = False
+###########################################################################################################################
+#                                                 EDIT THESE!                                                             #
+###########################################################################################################################
+    outdir = '/content/drive/My Drive/Colab Notebooks/styleganada-results/'
+    gpus = None # Number of GPUs: <int>, default = 1 gpu
+    snap = 1 # Snapshot interval: <int>, default = 50 ticks
+    seed = 1000
+    data = '/content/datasets/custom/'# Training dataset (required): <path>
+    res = None# Override dataset resolution: <int>, default = highest available
+    mirror =True# Augment dataset with x-flips: <bool>, default = False
+    metrics = []# List of metric names: [], ['fid50k_full'] (default), ...
+    metricdata = None# Metric dataset (optional): <path>
+    cfg = 'stylegan2'# Base config: 'auto' (default), 'stylegan2', 'paper256', 'paper512', 'paper1024', 'cifar', 'cifarbaseline'
+    gamma = None# Override R1 gamma: <float>, default = depends on cfg
+    kimg = 10000# Override training duration: <int>, default = depends on cfg
+    aug = 'ada' # Augmentation mode: 'ada' (default), 'noaug', 'fixed', 'adarv'
+    p = None# Specify p for 'fixed' (required): <float>
+    target = None # Override ADA target for 'ada' and 'adarv': <float>, default = depends on aug
+    augpipe = 'bgc'# Augmentation pipeline: 'blit', 'geom', 'color', 'filter', 'noise', 'cutout', 'bg', 'bgc' (default), ..., 'bgcfnc'
+    cmethod = None # Comparison method: 'nocmethod' (default), 'bcr', 'zcr', 'pagan', 'wgangp', 'auxrot', 'spectralnorm', 'shallowmap', 'adropout'
+    dcap = None # Multiplier for discriminator capacity: <float>, default = 1
+    augpipe = 'bgc'
+    resume = '/content/stylegan2-ffhq-config-f.pkl'# Load previous network: 'noresume' (default), 'ffhq256', 'ffhq512', 'ffhq1024', 'celebahq256', 'lsundog256', <file>, <url>
+    freezed = None # Freeze-D: <int>, default = 0 discriminator layers
 
-    # Metrics (not included in desc).
-    metrics    = [], # List of metric names: [], ['fid50k_full'] (default), ...
-    metricdata = None, # Metric dataset (optional): <path>
 
-    # Base config.
-    cfg        ='stylegan2', # Base config: 'auto' (default), 'stylegan2', 'paper256', 'paper512', 'paper1024', 'cifar', 'cifarbaseline'
-    gamma      = None, # Override R1 gamma: <float>, default = depends on cfg
-    kimg       = 5, # Override training duration: <int>, default = depends on cfg
+###########################################################################################################################
+#                                                 End of Edit Section                                                     #
+###########################################################################################################################
 
-    # Discriminator augmentation.
-    aug        =  'ada' , # Augmentation mode: 'ada' (default), 'noaug', 'fixed', 'adarv'
-    p          = None, # Specify p for 'fixed' (required): <float>
-    target     = None, # Override ADA target for 'ada' and 'adarv': <float>, default = depends on aug
-    augpipe    =  'bgc', # Augmentation pipeline: 'blit', 'geom', 'color', 'filter', 'noise', 'cutout', 'bg', 'bgc' (default), ..., 'bgcfnc'
+    tflib.init_tf({'rnd.np_random_seed': seed})
 
-    # Comparison methods.
-    cmethod    = None, # Comparison method: 'nocmethod' (default), 'bcr', 'zcr', 'pagan', 'wgangp', 'auxrot', 'spectralnorm', 'shallowmap', 'adropout'
-    dcap       = None, # Multiplier for discriminator capacity: <float>, default = 1
-
-    # Transfer learning.
-    resume     = None, # Load previous network: 'noresume' (default), 'ffhq256', 'ffhq512', 'ffhq1024', 'celebahq256', 'lsundog256', <file>, <url>
-    freezed    = None, # Freeze-D: <int>, default = 0 discriminator layers
-):
     # Initialize dicts.
     args = dnnlib.EasyDict()
     args.G_args = dnnlib.EasyDict(func_name='training.networks.G_main')
@@ -158,6 +160,7 @@ def setup_training_options(
         cfg = 'auto'
     assert isinstance(cfg, str)
     desc += f'-{cfg}'
+
     cfg_specs = {
         'auto':          dict(ref_gpus=-1, kimg=25000,  mb=-1, mbstd=-1, fmaps=-1,  lrate=-1,     gamma=-1,   ema=-1,  ramp=0.05, map=2), # populated dynamically based on 'gpus' and 'res'
         'stylegan2':     dict(ref_gpus=8,  kimg=25000,  mb=32, mbstd=4,  fmaps=1,   lrate=0.002,  gamma=10,   ema=10,  ramp=None, map=8), # uses mixed-precision, unlike original StyleGAN2
@@ -168,15 +171,12 @@ def setup_training_options(
         'cifarbaseline': dict(ref_gpus=2,  kimg=100000, mb=64, mbstd=32, fmaps=0.5, lrate=0.0025, gamma=0.01, ema=500, ramp=0.05, map=8),
     }
 
-
     assert cfg in cfg_specs
     spec = dnnlib.EasyDict(cfg_specs[cfg])
     if cfg == 'auto':
         desc += f'{gpus:d}'
         spec.ref_gpus = gpus
         spec.mb = max(min(gpus * min(4096 // res, 32), 64), gpus) # keep gpu memory consumption at bay
-#        spec.mb = max(min(gpus * min(4096 // res, 32), 64), gpus) # keep gpu memory consumption at bay
-#        spec.mb = max(min(gpus * min(4096 // res, 1), 1), gpus) # keep gpu memory consumption at bay
         spec.mbstd = min(spec.mb // gpus, 4) # other hyperparams behave more predictably if mbstd group size remains fixed
         spec.fmaps = 1 if res >= 512 else 0.5
         spec.lrate = 0.002 if res >= 1024 else 0.0025
@@ -408,14 +408,13 @@ def setup_training_options(
         desc += f'-freezed{freezed:d}'
         args.D_args.freeze_layers = freezed
 
-    return desc, args
+    return desc, args, outdir
 
 #----------------------------------------------------------------------------
 
-def run_training(outdir, seed, dry_run, **hyperparam_options):
-    # Setup training options.
-    tflib.init_tf({'rnd.np_random_seed': seed})
-    run_desc, training_options = setup_training_options(**hyperparam_options)
+def run_training():
+
+    run_desc, training_options, outdir = setup_training_options()
 
     # Pick output directory.
     prev_run_dirs = []
@@ -438,11 +437,6 @@ def run_training(outdir, seed, dry_run, **hyperparam_options):
     print(f'Resolution:        {training_options.train_dataset_args.resolution}')
     print(f'Number of GPUs:    {training_options.num_gpus}')
     print()
-
-    # Dry run?
-    if dry_run:
-        print('Dry run; exiting.')
-        return
 
     # Kick off training.
     print('Creating output directory...')
@@ -468,94 +462,8 @@ def _parse_comma_sep(s):
         return []
     return s.split(',')
 
-#----------------------------------------------------------------------------
-
-_cmdline_help_epilog = '''examples:
-
-  # Train custom dataset using 1 GPU.
-  python %(prog)s --outdir=~/training-runs --gpus=1 --data=~/datasets/custom
-
-  # Train class-conditional CIFAR-10 using 2 GPUs.
-  python %(prog)s --outdir=~/training-runs --gpus=2 --data=~/datasets/cifar10c \\
-      --cfg=cifar
-
-  # Transfer learn MetFaces from FFHQ using 4 GPUs.
-  python %(prog)s --outdir=~/training-runs --gpus=4 --data=~/datasets/metfaces \\
-      --cfg=paper1024 --mirror=1 --resume=ffhq1024 --snap=10
-
-  # Reproduce original StyleGAN2 config F.
-  python %(prog)s --outdir=~/training-runs --gpus=8 --data=~/datasets/ffhq \\
-      --cfg=stylegan2 --res=1024 --mirror=1 --aug=noaug
-
-available base configs (--cfg):
-  auto           Automatically select reasonable defaults based on resolution
-                 and GPU count. Good starting point for new datasets.
-  stylegan2      Reproduce results for StyleGAN2 config F at 1024x1024.
-  paper256       Reproduce results for FFHQ and LSUN Cat at 256x256.
-  paper512       Reproduce results for BreCaHAD and AFHQ at 512x512.
-  paper1024      Reproduce results for MetFaces at 1024x1024.
-  cifar          Reproduce results for CIFAR-10 (tuned configuration).
-  cifarbaseline  Reproduce results for CIFAR-10 (baseline configuration).
-
-transfer learning source networks (--resume):
-  ffhq256        FFHQ trained at 256x256 resolution.
-  ffhq512        FFHQ trained at 512x512 resolution.
-  ffhq1024       FFHQ trained at 1024x1024 resolution.
-  celebahq256    CelebA-HQ trained at 256x256 resolution.
-  lsundog256     LSUN Dog trained at 256x256 resolution.
-  <path or URL>  Custom network pickle.
-'''
-
-#----------------------------------------------------------------------------
-
 def main():
-    parser = argparse.ArgumentParser(
-        description='Train a GAN using the techniques described in the paper\n"Training Generative Adversarial Networks with Limited Data".',
-        epilog=_cmdline_help_epilog,
-        formatter_class=argparse.RawDescriptionHelpFormatter
-    )
-
-    group = parser.add_argument_group('general options')
-    group.add_argument('--outdir', help='Where to save the results (required)', required=True, metavar='DIR')
-    group.add_argument('--gpus', help='Number of GPUs to use (default: 1 gpu)', type=int, metavar='INT')
-    group.add_argument('--snap', help='Snapshot interval (default: 50 ticks)', type=int, metavar='INT')
-    group.add_argument('--seed', help='Random seed (default: %(default)s)', type=int, default=1000, metavar='INT')
-    group.add_argument('-n', '--dry-run', help='Print training options and exit', action='store_true', default=False)
-
-    group = parser.add_argument_group('training dataset')
-    group.add_argument('--data',   help='Training dataset path (required)', metavar='PATH', required=True)
-    group.add_argument('--res',    help='Dataset resolution (default: highest available)', type=int, metavar='INT')
-    group.add_argument('--mirror', help='Augment dataset with x-flips (default: false)', type=_str_to_bool, metavar='BOOL')
-
-    group = parser.add_argument_group('metrics')
-    group.add_argument('--metrics',    help='Comma-separated list or "none" (default: fid50k_full)', type=_parse_comma_sep, metavar='LIST')
-    group.add_argument('--metricdata', help='Dataset to evaluate metrics against (optional)', metavar='PATH')
-
-    group = parser.add_argument_group('base config')
-    group.add_argument('--cfg',   help='Base config (default: auto)', choices=['auto', 'stylegan2', 'paper256', 'paper512', 'paper1024', 'cifar', 'cifarbaseline'])
-    group.add_argument('--gamma', help='Override R1 gamma', type=float, metavar='FLOAT')
-    group.add_argument('--kimg',  help='Override training duration', type=int, metavar='INT')
-
-    group = parser.add_argument_group('discriminator augmentation')
-    group.add_argument('--aug',    help='Augmentation mode (default: ada)', choices=['noaug', 'ada', 'fixed', 'adarv'])
-    group.add_argument('--p',      help='Specify augmentation probability for --aug=fixed', type=float, metavar='FLOAT')
-    group.add_argument('--target', help='Override ADA target for --aug=ada and --aug=adarv', type=float)
-    group.add_argument('--augpipe', help='Augmentation pipeline (default: bgc)', choices=['blit', 'geom', 'color', 'filter', 'noise', 'cutout', 'bg', 'bgc', 'bgcf', 'bgcfn', 'bgcfnc'])
-
-    group = parser.add_argument_group('comparison methods')
-    group.add_argument('--cmethod', help='Comparison method (default: nocmethod)', choices=['nocmethod', 'bcr', 'zcr', 'pagan', 'wgangp', 'auxrot', 'spectralnorm', 'shallowmap', 'adropout'])
-    group.add_argument('--dcap',    help='Multiplier for discriminator capacity', type=float, metavar='FLOAT')
-
-    group = parser.add_argument_group('transfer learning')
-    group.add_argument('--resume',  help='Resume from network pickle (default: noresume)')
-    group.add_argument('--freezed', help='Freeze-D (default: 0 discriminator layers)', type=int, metavar='INT')
-
-    args = parser.parse_args()
-    try:
-        run_training(**vars(args))
-    except UserError as err:
-        print(f'Error: {err}')
-        exit(1)
+    run_training()
 
 #----------------------------------------------------------------------------
 
@@ -563,3 +471,7 @@ if __name__ == "__main__":
     main()
 
 #----------------------------------------------------------------------------
+
+
+
+
